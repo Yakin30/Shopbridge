dfdb2504becd55456a33578f647c5bcbb6a0436f
Lenovo <Lenovo@LAPTOP-VT80BD53>
2020-09-01 17:20:38 +0530

    Unittest changes

---
+++ ShopbProjtest/Controllers/ControllerTest.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShopbProj.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Web.Mvc;
+
+namespace ShopbProj.Controllers.Tests
+{
+    [TestClass()]
+    public class ControllerTest
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            ShopbridgesController controller = new ShopbridgesController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+
+            ShopbridgesController controller = new ShopbridgesController();
+
+            ViewResult result = controller.Create() as ViewResult;
+
+            Assert.IsNotNull(result);
+
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            ShopbridgesController controller = new ShopbridgesController();
+            
+            ViewResult result = controller.Edit(1) as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ ShopbProjtest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ShopbProjtest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ShopbProjtest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ed1d105b-340a-494c-9235-5b07b7b11fe1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
